2afba7b324474c209274f9e112433da160e76620
WubWub <desert.96@mail.ru>
2020-05-13 13:34:31 +0300

    add core v.3

---
+++ fallout-perks/src/ListOfSkills.java
@@ -0,0 +1,7 @@
+
+public class ListOfSkills {
+	public void getList(int[] arrayForListOfSkills) {
+		CharactersSkills charactersSkills = new CharactersSkills(arrayForListOfSkills[0], arrayForListOfSkills[1], arrayForListOfSkills[2], arrayForListOfSkills[3], arrayForListOfSkills[4], arrayForListOfSkills[5], arrayForListOfSkills[6]);
+		System.out.println(charactersSkills);
+	}
+}
