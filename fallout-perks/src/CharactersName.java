1c046c7a5980cd36f7b707e8f7f3318b1462ad21
WubWub <desert.96@mail.ru>
2020-05-13 12:45:14 +0300

    q

--- fallout-perks/src/CharactersName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class CharactersName {
-	public void setName() {
-		System.out.print("Name your character: ");
-		Scanner in = new Scanner(System.in);
-	    in.nextLine();
-	}
-}
