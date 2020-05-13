78b1b57464bb43827488f6f6020cd077aa787a11
WubWub <desert.96@mail.ru>
2020-05-13 13:46:07 +0300

    add core v.3

---
+++ fallout-perks/src/CharactersName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class CharactersName {
+	public void setName() {
+		System.out.print("Name your character: ");
+		Scanner in = new Scanner(System.in);
+	    in.nextLine();
+	}
+}
