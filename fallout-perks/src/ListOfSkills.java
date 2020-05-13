4a4843ec9f5a4d4fe5066746dfff4f3fe7fa26d7
WubWub <desert.96@mail.ru>
2020-05-13 13:44:24 +0300

    deleted

--- fallout-perks/src/ListOfSkills.java
+++
@@ -1,7 +0,0 @@
-
-public class ListOfSkills {
-	public void getList(int[] arrayForListOfSkills) {
-		CharactersSkills charactersSkills = new CharactersSkills(arrayForListOfSkills[0], arrayForListOfSkills[1], arrayForListOfSkills[2], arrayForListOfSkills[3], arrayForListOfSkills[4], arrayForListOfSkills[5], arrayForListOfSkills[6]);
-		System.out.println(charactersSkills);
-	}
-}
